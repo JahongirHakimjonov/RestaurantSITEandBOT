dd7eb70788952ea86199ef765a7e6aa16cf4ab45
Jahongir <jahongirhakimjonov@gmail.com>
2024-04-04 17:08:36 +0500

    commit

--- apps/restaurant/models.py
+++ apps/restaurant/models.py
@@ -177,12 +177,34 @@ class Menu(AbstractBaseModel):
 
 
 class Reservation(AbstractBaseModel):
+    GUESTS = (
+        (1, "1 Guest"),
+        (2, "2 Guests"),
+        (3, "3 Guests"),
+        (4, "4 Guests"),
+        (5, "5 Guests"),
+        (6, "6 Guests"),
+        (7, "7 Guests"),
+        (8, "8 Guests"),
+        (9, "9 Guests"),
+        (10, "10 Guests"),
+        (11, "11 Guests"),
+        (12, "12 Guests"),
+        (13, "13 Guests"),
+        (14, "14 Guests"),
+        (15, "15 Guests"),
+        (16, "16 Guests"),
+        (17, "17 Guests"),
+        (18, "18 Guests"),
+        (19, "19 Guests"),
+        (20, "20 Guests"),
+    )
     full_name = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
     date = models.DateField()
     time = models.TimeField()
-    guests = models.IntegerField()
+    guests = models.IntegerField(choices=GUESTS)
     message = models.TextField()
 
     class Meta:
